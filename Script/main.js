1cd1b66dbca81d2ee67c4434d4747d4aa6cf0dbf
lborgesc <lborgescaetano@gmail.com>
2022-05-06 16:30:31 -0300

    code correction

--- Script/main.js
+++ Script/main.js
@@ -1,210 +1,190 @@
 const form = document.querySelectorAll('.modal-field');
-const btnSalvar = document.querySelector('.salvar');
+let indexItem = 0; 
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('clientes')) || [];
-const setLocalStorage = (dbClientes) => localStorage.setItem('clientes', JSON.stringify(dbClientes));
+const getDatabase = () => JSON.parse(localStorage.getItem('clientes')) || [];
+const setDatabase = (dbClientes) => localStorage.setItem('clientes', JSON.stringify(dbClientes));
 
+function openModal ( ) {document.getElementById('modal').classList.add('active')};
 
-const openModal = () => {
-
-    document.getElementById('modal').classList.add('active')
-    document.querySelector('.modal-header h2').innerHTML = "Novo Cliente";
-}
-
-const closeModal = () => {
-    document.getElementById('modal').classList.remove('active')
+function closeModal ( )  {
+    document.getElementById('modal').classList.remove('active');
+    document.querySelector('header h2').innerHTML = "Novo Cliente"
     limparModal();
 }
 
 
-document.getElementById('cadastrarCliente').addEventListener('click', openModal)
-document.getElementById('modalClose').addEventListener('click', closeModal)
-
-
-
-
-
-const capturarDados = () => {
-    btnSalvar.addEventListener('click', () => {
-        if (document.querySelector('.modal-header h2').innerHTML === "Novo Cliente") {
-            const dbClientes = getLocalStorage();
-            dbClientes.push(clienteDados());
-            setLocalStorage(dbClientes);
-            update();
+function cadastrarCliente ( ) {
+    const database = getDatabase();
+    titulo = document.querySelector('.modal-header h2');
+    if (titulo.innerHTML === "Novo Cliente") {
+        if (capturarDados()) {
+            database.push(capturarDados());
+            setDatabase (database);
+            atualizarTela();
+            limparModal();
             closeModal();
         }
-    })
-}
-
-const editarApagar = () => { 
-
-    
-    window.addEventListener('click', (event) => {
-        const [action, index] = event.target.id.split('-');
-        if(event.target.type === 'button'){
-            if (action === 'editar') {
-                editarDados(index);
-            }
-            
-            if(action === 'excluir'){
-                const response = confirm(`Deseja realmente excluir ?`)
-                if (response) {
-                    deletarCadastro(index)
-                    update()
-                }
-            }
+    }
+    if (titulo.innerHTML === "Atualizar Cadastro") {
+        if (atualizarCadastro( indexItem ) === 'valido') {
+            atualizarCadastro( indexItem );
+            atualizarTela();
+            limparModal();
+            closeModal();
         }
-    })
-} 
-
-editarApagar();
-capturarDados();
-
-const deletarCadastro = (index) => {
-    const dbClientes = getLocalStorage();
-
-    dbClientes.splice(index, 1);
-    setLocalStorage(dbClientes)  
+    }
 }
 
-
-const editarDados = (index) =>{
-
-    const dbClientes = getLocalStorage();
-    document.getElementById('modal').classList.add('active')
-    document.querySelector('.modal-header h2').innerHTML = "Atualizar Cadastro";
-    
-    abriCadstro(dbClientes[index]);
-    editaCadastro(index)
-}
-
-const editaCadastro = (id) =>{
-
-    const dbClientes = getLocalStorage();
-
-    btnSalvar.addEventListener('click', ( ) => {
-
-        if(document.querySelector('.modal-header h2').innerHTML === "Atualizar Cadastro"){
-
-            form.forEach((dados) => {
-
-                switch (dados.getAttribute("placeholder")) {
-                    
-                case "Nome do Cliente":
-                    dbClientes[id].nome = dados.value; 
-                    break;
-    
-                case "e-mail do Cliente":
-                    dbClientes[id].email = dados.value;
-                    break;
-    
-                case "Celular do Cliente":
-                    dbClientes[id].celular = dados.value;
-                    break;
-    
-                case "Cidade do Cliente":
-                    dbClientes[id].cidade = dados.value;
-                    break;
-                }
-            })
+function capturarDados ( ) {
+    const dadosCliente  = {}
+    let dadosVazio = 0;
+    let celValido = true; 
+    form.forEach(dados => { 
+        if (!dados.value) {
+            dadosVazio++
         }
-        console.log(dbClientes)
-        atualizaTabela(dbClientes);
-    })
-} 
-
-const atualizaTabela = (dados) => {
-
-    setLocalStorage(dados);
-    update()
-    document.querySelector('#modal').classList.remove('active');
-}
-
-const abriCadstro = (cliente) => {
-    form.forEach(dados => {
         switch (dados.getAttribute("placeholder")) {
-
-            case "Nome do Cliente":
-                dados.value = cliente.nome;
-                break;
-
-            case "e-mail do Cliente":
-                dados.value = cliente.email;
-                break;
-
-            case "Celular do Cliente":
-                dados.value = cliente.celular;
-                break;
-
-            case "Cidade do Cliente":
-               dados.value =  cliente.cidade;
-                break; 
-        }
-    })
-}
-
-const limparTabela = () => {
-    const row = document.querySelectorAll("tbody>tr");
-    row.forEach(row => row.parentNode.removeChild(row));
-}
-
-const update = () => {
-
-    const dbClientes = getLocalStorage();
-    limparTabela();
-
-    dbClientes.forEach((cliente, index) => {
-        cliente.id = index;
-        criarElemento(cliente);
-    })
-}
-
-
-const criarElemento = (cliente) => {
-    return document.querySelector("tbody").innerHTML += `
-        <tr class="tabelCliente" id="${cliente.id}">
-            <td>${cliente.nome}</td>
-            <td>${cliente.email}</td>
-            <td>${cliente.celular}</td>
-            <td>${cliente.cidade}</td>
-            <td>
-                <button id="editar-${cliente.id}" type="button" class="button green">editar</button>
-                <button id="excluir-${cliente.id}" type="button" class="button red">excluir</button>
-            </td>
-        </tr>
-    `;
-};
-
-update();
-
-
-
-
-const clienteDados = () => {
-    const dadosCliente = {};
-    form.forEach(dados => {
-        switch (dados.getAttribute("placeholder")) {
-
             case "Nome do Cliente":
                 dadosCliente.nome = dados.value;
-                break;
-
+            break;`
+                `
             case "e-mail do Cliente":
                 dadosCliente.email = dados.value;
-                break;
-
+            break;    
             case "Celular do Cliente":
-                dadosCliente.celular = dados.value;
-                break;
-
+                dadosCliente.celular =  maskCel ( dados.value )  == ""? celValido = false : maskCel ( dados.value );
+            break;
             case "Cidade do Cliente":
                 dadosCliente.cidade = dados.value;
-                break;
-            }
-        })
-        return dadosCliente;
+            break;
+        }       
+    })
+    if (dadosVazio != 0) {
+        alert('Preencha todos os campos!!!');
+    }else if(celValido === false){
+        alert('Celular deve conter apenas números!!!');
+    }else{
+        return dadosCliente; 
+    }  
 }
 
-const limparModal = () => {
+function atualizarCadastro( index ){
+    let dadosVazio = 0;
+    let celValido = true; 
+    const database = getDatabase();
+    form.forEach(dados => { 
+        if (!dados.value) {
+            dadosVazio++
+        }
+            switch (dados.getAttribute("placeholder")) {
+                case "Nome do Cliente":
+                    database[index].nome = dados.value;
+                break;`
+                    `
+                case "e-mail do Cliente":
+                    database[index].email = dados.value;
+                break;    
+                case "Celular do Cliente":
+                    database[index].celular = maskCel ( dados.value )  == ""? celValido = false : maskCel ( dados.value );
+                break;
+                case "Cidade do Cliente":
+                    database[index].cidade = dados.value;
+                break;   
+        }
+    })
+    if (dadosVazio != 0) {
+        alert('Preencha todos os campos!!!');
+    }else if(celValido === false){
+        alert('Celular deve conter apenas números!!!');
+    }else{
+        setDatabase (database);
+        return 'valido'; 
+    } 
+}
+
+function criarElemento (cliente, id) {
+    let trItem = document.createElement('tr');
+    trItem.classList.add('tabelCliente'); 
+    trItem.id = id; 
+    trItem.innerHTML = `
+    <tr class="tabelCliente" id="${id}">
+        <td>${cliente.nome}</td>
+        <td>${cliente.email}</td>
+        <td>${cliente.celular}</td>
+        <td>${cliente.cidade}</td>
+        <td>
+            <button id="editar" type="button" class="button green" data-index="${id}" onclick="editarItem(this)">editar</button>
+            <button id="excluir" type="button" class="button red" data-index="${id}" onclick="removerItem(this)">excluir</button>
+        </td>
+    </tr>
+`
+    document.querySelector("tbody").appendChild(trItem);
+};
+
+function editarItem( index ) {
+    indexItem = index.dataset.index;
+    const database = getDatabase();
+    document.querySelector('header h2').innerHTML = "Atualizar Cadastro"
+    openModal( );
+    database[indexItem]
+    carregarDados(database[indexItem]);
+}
+
+function carregarDados( dadosCliente ) {
+    form.forEach(dados => { 
+        switch (dados.getAttribute("placeholder")) {
+            case "Nome do Cliente":
+                dados.value = dadosCliente.nome;
+            break;`
+                `
+            case "e-mail do Cliente":
+                dados.value = dadosCliente.email;
+            break;    
+            case "Celular do Cliente":
+                dados.value = dadosCliente.celular;
+            break;
+            case "Cidade do Cliente":
+                dados.value = dadosCliente.cidade;
+            break;
+        }       
+    })
+}
+
+function removerItem( index ) {
+    const database = getDatabase();
+    const id = index.dataset.index;
+    database.splice(id, 1);
+    setDatabase (database);
+    atualizarTela();
+}
+
+function atualizarTela ( ) {
+    limparItems();
+    const database = getDatabase();
+    database.forEach((dados, index) => criarElemento(dados, index))
+}
+
+function limparModal ( ) {
     form.forEach(dados => dados.value = '');
 }
+
+function limparItems ( ) {
+    const item = document.querySelector("tbody");
+    while (item.firstChild){
+        item.removeChild(item.lastChild);
+    }
+}
+
+
+function maskCel ( cel ) {
+    
+    return cel
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{4})\d+?$/, '$1')
+}
+
+atualizarTela();
+
+
